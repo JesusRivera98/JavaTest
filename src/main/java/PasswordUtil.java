52c947a66d259bad1c2e083920539fbad5e08012
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-05 15:12:51 -0500

    Start the project with Maven and junit

---
+++ src/main/java/PasswordUtil.java
@@ -0,0 +1,8 @@
+public class PasswordUtil {
+    public enum SecurityLevel{
+        WEAK, MEDIUM, STRONG
+    }
+    public SecurityLevel assesPassword(String password){
+        return null;
+    }
+}
---
+++ src/test/java/PasswordUtilTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+    @Test
+    public void weak_when_has_les_than_8_letter() {
+
+    }
+}
