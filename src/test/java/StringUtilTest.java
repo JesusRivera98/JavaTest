0414e8586d06d6f30e364fd90fb03d083123bbc9
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-06 15:53:34 -0500

    Change to JUnit

--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -1,15 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("hello", 3), "hellohellohello");
-
-        assertEquals(StringUtil.repeat("hello", 1), "hello");
-    }
-
-    private static void assertEquals(String current, String expected){
-        if(!current.equals(expected)){
-            throw new RuntimeException(current + " is not equal to " + expected);
-        }
+    @Test
+    public void testRepeat() {
+        Assert.assertEquals("hellohellohello", StringUtil.repeat("hello", 3));
+        Assert.assertEquals("hello", StringUtil.repeat("hello", 1));
     }
 }
