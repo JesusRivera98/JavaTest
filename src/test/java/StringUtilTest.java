c6cc136f3a6a2e55df1a13621b45219f560046c0
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-06 15:37:45 -0500

    Change to detect error

--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -4,14 +4,14 @@ public class StringUtilTest {
     public static void main(String[] args) {
         String result = StringUtil.repeat("hello", 3);
         System.out.println(result);
-        if (result.equals("hellohellohello")){
-            System.out.println("OK");
+        if (!result.equals("hellohellohello")){
+            System.out.println("ERROR");
         }
 
         String result2 = StringUtil.repeat("hello", 1);
         System.out.println(result2);
-        if (result2.equals("hello")){
-            System.out.println("OK");
+        if (!result2.equals("hello")){
+            System.out.println("ERROR");
         }
     }
 }
