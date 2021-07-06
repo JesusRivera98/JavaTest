5ae56d96d8f21d456dc7d4456a16a5ce2d0bb39e
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-06 15:39:09 -0500

    Change to exception

--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -5,13 +5,13 @@ public class StringUtilTest {
         String result = StringUtil.repeat("hello", 3);
         System.out.println(result);
         if (!result.equals("hellohellohello")){
-            System.out.println("ERROR");
+            throw new RuntimeException("ERROR");
         }
 
         String result2 = StringUtil.repeat("hello", 1);
         System.out.println(result2);
         if (!result2.equals("hello")){
-            System.out.println("ERROR");
+            throw new RuntimeException("ERROR");
         }
     }
 }
