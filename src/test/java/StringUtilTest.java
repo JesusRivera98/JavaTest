8ee7072872ea5ff5b67ad97045e80d4978e9850e
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-06 16:01:45 -0500

    Change structure to JUnit

--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -5,8 +5,19 @@ import static org.junit.Assert.*;
 
 public class StringUtilTest {
     @Test
-    public void testRepeat() {
-        Assert.assertEquals("hellohellohello", StringUtil.repeat("hello", 3));
+    public void repeat_string_once() {
         Assert.assertEquals("hello", StringUtil.repeat("hello", 1));
+    }
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("hellohellohello", StringUtil.repeat("hello", 3));Assert.assertEquals("hello", StringUtil.repeat("hello", 1));
+    }
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hello", 0));
+    }
+    @Test(expected =  IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hello", -1);
     }
 }
