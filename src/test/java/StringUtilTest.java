2c0d1a729ce2d516c85449b7c1bbfb9de238af52
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-06 15:25:43 -0500

    Add two basic tests with OK

---
+++ src/test/java/StringUtilTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("hello", 3);
+        System.out.println(result);
+        if (result.equals("hellohellohello")){
+            System.out.println("OK");
+        }
+
+        String result2 = StringUtil.repeat("hello", 1);
+        System.out.println(result2);
+        if (result2.equals("hello")){
+            System.out.println("OK");
+        }
+    }
+}
