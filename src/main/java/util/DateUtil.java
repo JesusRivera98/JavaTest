9d97d66dc92b28652a43737f15784058f229a420
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-07 10:19:57 -0500

    add not 4 test and refactor function

--- src/main/java/util/DateUtil.java
+++ src/main/java/util/DateUtil.java
@@ -4,10 +4,6 @@ package util;
 public class DateUtil {
 
     public static boolean isLeapYear(int year){
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-            return true;
-        }else {
-            return false;
-        }
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
