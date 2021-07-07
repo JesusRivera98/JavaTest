50fa61d55e3fec708038941e74876da0de61782e
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-07 10:48:25 -0500

    Add FizzBuzz Fizz 3

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,6 @@
+public class FizzBuzz {
+
+    public static String fizzBuzz(int n) {
+        return "Fizz";
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+    @Test
+    public void fizz_in_divisible_by_3() {
+        assertEquals(FizzBuzz.fizzBuzz(3), "Fizz");
+    }
+
+    public void buzz_in_divisible_by_5() {
+    }
+
+    public void fizzBuzz_in_divisible_by_3_and_5() {
+    }
+}
